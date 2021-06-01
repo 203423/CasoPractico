1fe3a8ad86436c28fc9005f9e18c99636ee363c8
203462 <203462@ids.upchiapas.edu.mx>
2021-05-31 19:55:28 -0500

    Clase Regla

---
+++ Regla.java
@@ -0,0 +1,26 @@
+public class Regla {
+    private String regla;
+    private String clave;
+
+    public Regla(String regla, String clave) {
+        this.regla = regla;
+        this.clave = clave;
+    }
+
+    public String getRegla() {
+        return this.regla;
+    }
+
+    public void setRegla(String regla) {
+        this.regla = regla;
+    }
+
+    public String getClave() {
+        return this.clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+}
